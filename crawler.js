964c9a1010dcc2b4c833d5c8a28390122ab0f441
mzmero <m.zmiro@hotmail.com>
2022-03-17 17:45:07 +0200

    save image to json

--- crawler.js
+++ crawler.js
@@ -1,14 +1,52 @@
 import axios from 'axios'
-
-
-const craw =async ({url})=>{
-    const response= await axios.get(url)
-    .then(({data})=>{
-       return data;
-    }).catch( (error)=> {
-        console.log(error);
-      });
-    console.log(response)
+import * as Cheerio from 'cheerio';
+import fs from 'fs'
+const maxDepth = 3;
+const seenURL=[]
+const results = []
+function getURL({url,link}){
+    if(link.includes('http'))
+        return link;
+    else if(link.startsWith('/')){
+        return url+link;
+    }
 }
 
-craw({"url":"http://crawler-test.com/redirects/redirect_target"})
+async function add_images(images,url,depth){
+    images.forEach((img,i)=>{
+        const _path = url + img
+        const temp= JSON.stringify({"imageUrl":_path,"sourceUrl":url,"depth":depth})
+        fs.writeFileSync("results.json", temp);
+        //results.push({"imageUrl":_path,"sourceUrl":url,"depth":depth})
+    })
+}
+
+const crawl = async ({url,depth})=>{
+    if(depth > maxDepth || seenURL[url])
+        return 
+    seenURL[url] =true;
+    const html = await axios.get(url)
+    .then(({data})=>{
+       return data;
+    })
+    .catch((error)=> {
+        console.log(error.message);
+    });
+    if(typeof html ==="string"){
+        const $ = Cheerio.load(html)
+        const images = $("img").map((i,img)=>img.attribs.src).get()
+        if(images.length > 0)
+           await add_images(images,url,depth)
+        const links = $("a").map((i,link)=>link.attribs.href).get()
+        links.forEach((link,i)=>{
+            crawl({"url":getURL({"url":url,"link":link}),"depth":depth+1})
+        })
+    }
+    return ;
+}
+
+ function start (url){
+   crawl({"url":originURL ,"depth":0})
+}
+const originURL= process.argv[2]
+start(originURL)
