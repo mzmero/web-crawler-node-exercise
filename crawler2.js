74b7f5b7f256db09947e9580020cccb43ffbf33a
mzmero <m.zmiro@hotmail.com>
2022-03-17 22:53:25 +0200

    another approach

--- crawler2.js
+++ crawler2.js
@@ -7,6 +7,7 @@ import * as urlParser from "url";
 const maxDepth = 3;
 const seenURL=[]
 
+
 //check url and apply a change depend on what type it is
 function getURL({host,protocol,link}){
     if(link.includes('http'))
@@ -18,16 +19,25 @@ function getURL({host,protocol,link}){
   }
 }
 
-//add images to result
-function add_images(images,url,depth,stackObj){
+
+//because its a recursive function , read from json and apply changes to it
+async function add_images(images,url,depth){
     const { host, protocol } = urlParser.parse(url);
     images.forEach((img,i)=>{
         if(img.length>0){
             let _path;
-        if(!img.startsWith("data:image"))
-            _path = getURL({"host":host,"protocol":protocol,"link":img})
-        else _path = img;
-        stackObj.results.push({"imageUrl":_path,"sourceUrl":url,"depth":depth})
+            if(!img.startsWith("data:image"))
+                _path = getURL({"host":host,"protocol":protocol,"link":img})
+            else _path = img;
+            try {
+                const rawdata = fs.readFileSync('results2.json');
+                const imagesObj = JSON.parse(rawdata);
+                imagesObj.push({"imageUrl":_path,"sourceUrl":url,"depth":depth})
+                const temp= JSON.stringify(imagesObj)
+                fs.writeFileSync("results2.json", temp);
+            } catch (error) {
+                console.log(error)
+            }
         }
     })
 }
@@ -35,48 +45,42 @@ function add_images(images,url,depth,stackObj){
 /*
 check if depth doesn't exceed the maxDepth and if we visited the url before
 fetch html and use cheerio to extract all links and images
-call for save image function and add all new links to stack
+call for save image function and call for each link recursively
 */
-const crawl = async ({url,depth,stackObj})=>{
+
+const crawl = async ({url,depth})=>{
     if(depth > maxDepth || seenURL[url])
         return 
     seenURL[url] =true;
+    console.log(url)
     const html = await axios.get(url)
     .then(({data})=>{
        return data;
     })
     .catch((error)=> {
-        //console.log(url)
-        console.log(error.message);
+        //console.log(error.message);
     });
     if(typeof html ==="string"){
         const $ = Cheerio.load(html)
         const images = $("img").map((i,img)=>img.attribs.src).get()
         if(images.length > 0)
-            add_images(images,url,depth,stackObj)
+           await add_images(images,url,depth)
         const links = $("a").map((i,link)=>link.attribs.href).get()
         const { host, protocol } = urlParser.parse(url);
         links.forEach((link,i)=>{
-            stackObj.stack.push({"url":getURL({"host":host,"protocol":protocol,"link":link}),"depth":depth+1})
+            crawl({"url":getURL({"host":host,"protocol":protocol,"link":link}),"depth":depth+1})
         })
     }
+    return ;
 }
 
-/*
-create obj to save result and links
-as long as there are links left pop and call for a link
-when there is no links left save the result in results.json
-*/
-async function start (originURL){
-    const stackObj = {"stack":[],"results":[]}
-    stackObj.stack.push({"url":originURL,"depth":0})
-    while(stackObj.stack.length>0){
-        const {url,depth} = stackObj.stack.pop()
-        await crawl({"url":url ,"depth":depth,"stackObj":stackObj})
-    }
-    const temp= JSON.stringify(stackObj.results)
-    fs.writeFileSync("results.json", temp);
-
+//call for first url recursively
+function start (url){
+   crawl({"url":url ,"depth":0})
 }
+//cli url
 const originURL= process.argv[2]
+
+// open/overwrite result.json
+fs.writeFileSync('results2.json', JSON.stringify([]))
 start(originURL)
